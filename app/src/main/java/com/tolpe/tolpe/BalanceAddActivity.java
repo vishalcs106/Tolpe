59a5656688d9442db4d7a5a49c6c1b5c1025743d
vishal <vishalcs106@rocketmail.com>
2016-01-09 13:48:58 +0530

    Activity created

---
+++ app/src/main/java/com/tolpe/tolpe/BalanceAddActivity.java
@@ -0,0 +1,15 @@
+package com.tolpe.tolpe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dell 3450 on 1/9/2016.
+ */
+public class BalanceAddActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balance_add);
+    }
+}
--- app/src/main/java/com/tolpe/tolpe/MainActivity.java
+++ app/src/main/java/com/tolpe/tolpe/MainActivity.java
@@ -1,7 +1,8 @@
 package com.tolpe.tolpe;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,7 +10,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +21,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        context = this;
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(context, BalanceAddActivity.class);
+                startActivity(i);
             }
         });
     }
@@ -46,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
